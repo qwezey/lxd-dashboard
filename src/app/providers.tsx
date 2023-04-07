4f2991daa6e6fd0f9361184e6b5cc55b8a5062f7
Ammaar Muhammad Iqbal <ammaar.iqbal@gmail.com>
2023-04-07 14:59:58 -0400

    migrate to non pages dir

--- src/app/providers.tsx
+++
@@ -1,8 +0,0 @@
-'use client';
-
-import {FC, PropsWithChildren} from 'react';
-import {SessionProvider} from 'next-auth/react';
-
-export function Providers({children}: PropsWithChildren): ReturnType<FC> {
-  return <SessionProvider>{children}</SessionProvider>;
-}
